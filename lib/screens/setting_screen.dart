bd799669a0704c1d57e6900024814a14d3c391c0
Haythem Dridi <haithemdridiweb@gmail.com>
2023-04-12 04:56:38 +0100

    logout button

--- lib/screens/setting_screen.dart
+++ lib/screens/setting_screen.dart
@@ -1,14 +1,26 @@
+import 'package:e_commerce_firebase_getw/controllers/auth_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:get/get.dart';
 
 class SettingScreen extends StatelessWidget {
-  const SettingScreen({super.key});
+  SettingScreen({super.key});
+
+  final _authController = Get.find<AuthController>();
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('Setting'),
-    );
+        child: Column(
+      children: [
+        ElevatedButton(
+          onPressed: () {
+            _authController.logout();
+          },
+          child: Text('Logout'),
+        )
+      ],
+    ));
   }
 }
