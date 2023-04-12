d4a6ac798d75e385e47f0ee8c2fbefa04c1b19cb
Haythem Dridi <haithemdridiweb@gmail.com>
2023-04-12 08:42:27 +0100

    Load Favorites From Storage

--- lib/controllers/product_controller.dart
+++ lib/controllers/product_controller.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
+
 import 'package:e_commerce_firebase_getw/models/product_model.dart';
 import 'package:e_commerce_firebase_getw/services/product_service.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 
 class ProductController extends GetxController {
   RxList<Product> products = <Product>[].obs;
@@ -8,10 +11,15 @@ class ProductController extends GetxController {
 
   RxBool isLoading = true.obs;
 
-  void onInit() {
+  var storage = GetStorage();
+
+  void onInit() async {
     super.onInit();
 
     getProducts();
+
+    var favorites = await storage.read('favorites');
+    this.favorites.addAll(Product.productsFromJson(favorites).toList());
   }
 
   getProducts() async {
@@ -24,15 +32,18 @@ class ProductController extends GetxController {
     isLoading(false);
   }
 
-  toggleFavorites(Product product) {
+  toggleFavorites(Product product) async {
     if (isFavorite(product)) {
       favorites.remove(product);
     } else {
       favorites.add(product);
     }
+
+    await storage.write('favorites',
+        jsonEncode(favorites.map((element) => element.toJson()).toList()));
   }
 
   bool isFavorite(Product product) {
-    return favorites.contains(product);
+    return favorites.where((p) => p.id == product.id).isNotEmpty;
   }
 }
